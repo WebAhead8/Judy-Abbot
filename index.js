ffaf4fc25c0f5e9e20eb41c06c744370f6eecb88
AladdinHammodi <Aladdin.Hammodi@gmail.com>
2021-01-19 12:28:36 +0200

    up

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const list = [{todo: "drink Coffe"}, {todo: "shower"}, {todo: "drink cola"}, {todo: "eat vegtables"}, {todo: "read book"}]
+
+function addNewMission (){
+    pass
+}
+
+function editMission(){
+    pass
+}
+
+function deleteMission(){
+    pass
+}
+
+function checkMission(){
+    pass
+}
+
+function categorizeMission(){
+    pass
+}
+
+function showOnCallendar(){
+    pass
+}
+
+function remindMe(){
+    pass
+}
+
+function filter(){
+    pass
+}
