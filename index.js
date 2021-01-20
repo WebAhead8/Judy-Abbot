91256bcafcab1c2ede62f9da668bf77bf9ca3129
mahmoud Diab <mhmoddiab2@gmail.com>
2021-01-20 18:18:59 +0200

    add tests

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
 // Select the Elements
-const clear = document.querySelector('.clear')
-const dateElement = document.getElementById('date')
-const list = document.getElementById('list')
-const input = document.getElementById('input')
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
 
 // Variables
-var LIST, id
+var LIST, id;
 
 //classes names
-const CHECK = 'fa-check-circle'
-const UNCHECK = 'fa-circle-thin'
-const LINE_THROUGH = 'lineThrough'
-const SAVE = 'fa fa-save deee'
-const EDIT = 'fa fa-edit de'
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+const SAVE = "fa fa-save deee";
+const EDIT = "fa fa-edit de";
 
 // get item from localstorage
-let data = localStorage.getItem('TODO')
-console.log(data, 'data')
+let data = localStorage.getItem("TODO");
 // check if data is not empty
 if (data) {
-  LIST = JSON.parse(data)
-  id = LIST.length // set the id to the last one in the list
-  loadList(LIST) // load the list to the user interface
+  LIST = JSON.parse(data);
+  id = LIST.length; // set the id to the last one in the list
+  loadList(LIST); // load the list to the user interface
 } else {
   // if data isn't empty
-  LIST = []
-  id = 0
+  LIST = [];
+  id = 0;
 }
 
 // load items to the user's interface
-function loadList (array) {
+function loadList(array) {
   array.forEach(function (item) {
-    addNewMission(item.name, item.id, item.done, item.trash)
-  })
+    addNewMission(item.name, item.id, item.done, item.trash);
+  });
 }
 
 // clear the local storage
-clear.addEventListener('click', function () {
-  localStorage.clear()
-  location.reload()
-})
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
 
 // Show todays date
-const options = { weekday: 'long', month: 'short', day: 'numeric' }
-const today = new Date()
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
 
-dateElement.innerHTML = today.toLocaleDateString('en-US', options)
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 //const list = [{todo: "drink Coffe", id:}, {todo: "shower"}, {todo: "drink cola"}, {todo: "eat vegtables"}, {todo: "read book"}]
 
 // Adding Todo to the list :------->
-function addNewMission (toDo, id, done, trash) {
+function addNewMission(toDo, id, done, trash) {
   // if the toDo is in the trash, don't allow the user from add it again!
   if (trash) {
-    return
+    return;
   }
 
-    if (done) {
+  if (done) {
     var DONE = CHECK;
     var LINE = LINE_THROUGH;
   } else {
@@ -73,80 +72,74 @@ function addNewMission (toDo, id, done, trash) {
     </div>
     <i class="fa fa-edit dee" job="edit" id="${id}"></i>
     <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
-  </li>`
+  </li>`;
 
-  list.insertAdjacentHTML('beforeend', item) //add new element at the end of the list in the html page ul
+  list.insertAdjacentHTML("beforeend", item); //add new element at the end of the list in the html page ul
 }
 
 // add an item to the list with the enter key
-document.addEventListener('keypress', function (event) {
-  if (event.key === 'Enter') {
-    const toDo = input.value
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    const toDo = input.value;
     // checks if the input isn't empty--->
 
     if (toDo) {
-      addNewMission(toDo, id) // send the input to the add function to add the job to the frontend(for the user)
-      LIST.push({ name: toDo, id: id, done: false, trash: false }) //add the job info to the array list in  the backend(for the devloper)
-      console.log(LIST)
-      id++ //increase the id by one every time there is a new job
+      addNewMission(toDo, id); // send the input to the add function to add the job to the frontend(for the user)
+      LIST.push({ name: toDo, id: id, done: false, trash: false }); //add the job info to the array list in  the backend(for the devloper)
+      id++; //increase the id by one every time there is a new job
     }
-    input.value = ''
+    input.value = "";
   }
-})
+});
 
 // function editMission(){
 //     pass
 // }
 
-function deleteMission (element) {
-  element.parentNode.parentNode.removeChild(element.parentNode)
-  console.log(element.id)
+function deleteMission(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
 
-  LIST[element.id].trash = true
+  LIST[element.id].trash = true;
 }
 
-function checkMission (element) {
-  element.classList.toggle(CHECK)
-  element.classList.toggle(UNCHECK)
-  element.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH)
-  LIST[element.id].done = LIST[element.id].done ? false : true
+function checkMission(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
 // edit to do
-function editMission (element) {
-  let num = element.id
-  console.log(num)
-  const index = LIST.findIndex(text => text.id == num)
-  console.log(index)
-  document.getElementsByClassName('edit')[index].style.display = 'flex'
-  console.log('div display')
+function editMission(element) {
+  let num = element.id;
+  const index = LIST.findIndex((text) => text.id == num);
+  document.getElementsByClassName("edit")[index].style.display = "flex";
 }
 // save to do
-function saveMission (element) {
-  let num1 = element.id
-  const index1 = LIST.findIndex(text => text.id == num1)
-  let oldInputValue = document.getElementsByClassName('text')[index1]
-  let newInputValue = element.previousSibling.previousSibling
-  oldInputValue.textContent = newInputValue.value
-  newInputValue.value = ''
-  document.getElementsByClassName('edit')[index1].style.display = 'none'
+function saveMission(element) {
+  let num1 = element.id;
+  const index1 = LIST.findIndex((text) => text.id == num1);
+  let oldInputValue = document.getElementsByClassName("text")[index1];
+  let newInputValue = element.previousSibling.previousSibling;
+  oldInputValue.textContent = newInputValue.value;
+  LIST[index1].name = newInputValue.value;
+  newInputValue.value = "";
+  document.getElementsByClassName("edit")[index1].style.display = "none";
 }
 
-list.addEventListener('click', function (event) {
-  const element = event.target // return the clicked element inside list
-  const elementJob = element.attributes.job.value // complete or delete
-  console.log(element)
-  if (elementJob == 'complete') {
-    checkMission(element)
-  } else if (elementJob == 'delete') {
-    deleteMission(element)
-  } else if (elementJob === 'edit') {
-    editMission(element)
-  } else if (elementJob === 'save') {
-    saveMission(element)
+list.addEventListener("click", function (event) {
+  const element = event.target; // return the clicked element inside list
+  const elementJob = element.attributes.job.value; // complete or delete
+  if (elementJob == "complete") {
+    checkMission(element);
+  } else if (elementJob == "delete") {
+    deleteMission(element);
+  } else if (elementJob === "edit") {
+    editMission(element);
+  } else if (elementJob === "save") {
+    saveMission(element);
   }
 
   // add item to localstorage ( this code must be added where the LIST array is updated)
-  localStorage.setItem('TODO', JSON.stringify(LIST))
-})
-
+  localStorage.setItem("TODO", JSON.stringify(LIST));
+});
---
+++ index.test.js
@@ -0,0 +1,56 @@
+test("check if the push (enter) is working ", (t) => {
+  //   function stamiulate enter pressing to save the input.value
+  input.value = "a";
+  const ke = new KeyboardEvent("keypress", {
+    bubbles: true,
+    cancelable: true,
+    key: "Enter",
+  });
+  input.dispatchEvent(ke);
+  //   addNewMission(toDo);
+  t.equal(LIST[0].name, "a");
+});
+
+test("check if the push (enter) is working ", (t) => {
+  //   function stamiulate enter pressing to save the input.value
+  input.value = "aaa";
+  const ke = new KeyboardEvent("keypress", {
+    bubbles: true,
+    cancelable: true,
+    key: "Enter",
+  });
+  input.dispatchEvent(ke);
+  //   addNewMission(toDo);
+  t.equal(LIST[1].name, "aaa");
+});
+
+test("check if check function works", (t) => {
+  const mission = document.querySelector(".item i");
+  mission.click();
+  const expected = "fa co fa-check-circle";
+  t.equal(mission.className, expected);
+});
+
+test("check if uncheck function works", (t) => {
+  const unmission = document.querySelector(".item i");
+  unmission.click();
+  const expected = "fa co fa-circle-thin";
+  t.equal(unmission.className, expected);
+});
+
+test("cheack if edit function works", (t) => {
+  const editFun = document.querySelector(".item").childNodes[7];
+  editFun.click();
+  const expected = "flex";
+  styleEdit = document.querySelector(".edit").style.display;
+  t.equal(styleEdit, expected);
+});
+
+test("check if save function works", (t) => {
+  const input1 = document.querySelector(".edit").childNodes[1];
+  input1.value = "9899";
+  const saveFun = document.querySelector(".edit").childNodes[3];
+  saveFun.click();
+  const expected = "9899";
+  t.equal(LIST[0].name, expected);
+});
